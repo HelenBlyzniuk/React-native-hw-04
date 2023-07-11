72987e076abc24dbae36ad734ef833c37a519a80
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-11 22:02:07 +0300

    Home, Map

---
+++ React-native-hw-04/screens/HomeScreen/Home.js
@@ -0,0 +1,13 @@
+import {
+    View,
+  } from "react-native";
+
+export function Home() {
+   
+  
+    return (
+      <View style={styles.post_page_container}>
+       
+      </View>
+    );
+  }
---
+++ React-native-hw-04/screens/MapScreen/MapScreen.js
@@ -0,0 +1,14 @@
+import {
+    View,
+  } from "react-native";
+
+export function MapScreen() {
+   
+  
+    return (
+      <View style={styles.post_page_container}>
+       
+      </View>
+    );
+  }
+  
