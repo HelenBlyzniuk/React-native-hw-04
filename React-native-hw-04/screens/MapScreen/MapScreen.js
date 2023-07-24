47d708d3f83c8327b171e6468998583abb2ea503
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-24 09:53:16 +0300

    mapScreen

--- React-native-hw-04/screens/MapScreen/MapScreen.js
+++ React-native-hw-04/screens/MapScreen/MapScreen.js
@@ -1,14 +1,45 @@
 import {
-    View,Text
+    View,StyleSheet
   } from "react-native";
 
 export function MapScreen() {
    
   
     return (
-      <View style={styles.post_page_container}>
-        <Text>MapScreen</Text>
+      <View style={styles.container}>
+        <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        minZoomLevel={15}
+        onMapReady={() => console.log("Map is ready")}
+        onRegionChange={() => console.log("Region change")}
+      >
+        <Marker
+          title="I am here"
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          description='Hello'
+        />
+      </MapView>
       </View>
     );
   }
   
+
+  const styles=StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    mapStyle: {
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").height,
+    },
+  })
