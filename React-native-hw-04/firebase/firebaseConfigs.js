f6d9b7727a3cbc488d6fda3d3acfee9847fafa2c
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-08-03 18:06:30 +0300

    firebase is not ready

--- React-native-hw-04/firebase/firebaseConfigs.js
+++ React-native-hw-04/firebase/firebaseConfigs.js
@@ -1,13 +1,23 @@
 
-
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: 'react-native-57242',
+    apiKey: '606cb0045cb7a535b4947e0814505a56d37417e1',
     authDomain: 'react-native-57242.firebaseapp.com',
     databaseURL: 'https://react-native-57242.firebaseio.com',
-    projectId: 'project-id',
-    storageBucket: 'project-id.appspot.com',
+    projectId: 'react-native-57242',
+    storageBucket: 'react-native-57242.appspot.com',
     messagingSenderId: 'sender-id',
     appId: 'app-id',
     measurementId: 'G-measurement-id',
   };
+  
+
+  сonst app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
